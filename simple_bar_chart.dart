f49da85271315cfe2af172dfd4204fb543d89aa6
Aakash Kondhalkar <43432493+aakashkondhalkar@users.noreply.github.com>
2023-08-06 17:36:16 +0530

    Update simple_bar_chart.dart

--- simple_bar_chart.dart
+++ simple_bar_chart.dart
@@ -1,45 +1,26 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
-const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
-
-void main() {
-  runApp(MyApp());
-}
-
-class Data {
-  int x;
-  int y;
-  Data(this.x, this.y);
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final List<int> data = [50, 100, 75, 150, 80];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: darkBlue,
-      ),
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: BarChart(),
-      ),
-    );
-  }
-}
-
-class BarChart extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final double width = MediaQuery.of(context).size.width;
-    final double height = MediaQuery.of(context).size.height;
-
-    List<int> data = [4, 5, 8, 2, 9, 1, 3, 1, 6, 7];
-    return Container(
-      padding: const EdgeInsets.all(16),
-      child: CustomPaint(
-        size: Size(width, height),
-        painter: BarChartPainter(data: data),
+        appBar: AppBar(
+          title: Text('Bar Chart with Custom Painter'),
+        ),
+        body: Center(
+          child: Container(
+            width: 500,
+            height: 400,
+            child: CustomPaint(
+              painter: BarChartPainter(data),
+            ),
+          ),
+        ),
       ),
     );
   }
@@ -48,76 +29,57 @@ class BarChart extends StatelessWidget {
 class BarChartPainter extends CustomPainter {
   final List<int> data;
 
-  BarChartPainter({required this.data});
+  BarChartPainter(this.data);
 
   @override
   void paint(Canvas canvas, Size size) {
-    var coordinateLinePaint = Paint()
-      ..color = Colors.white
-      ..strokeWidth = 2;
+    final double chartWidth = size.width;
+    final double chartHeight = size.height;
+    final double barWidth = chartWidth / data.length;
+    final int maxBarHeight = data.reduce((max, value) => max > value ? max : value);
+    final double unitHeight = chartHeight / maxBarHeight;
 
-    var barPaint = Paint()
+    Paint paint = Paint()
       ..color = Colors.blue
       ..style = PaintingStyle.fill;
-    var barPaint2 = Paint()..color = Colors.transparent;
 
-    var vLineStart = Offset(0, size.height);
-    var vLineEnd = const Offset(0, 0);
-
-    var hLineStart = Offset(0, size.height);
-    var hLineEnd = Offset(size.width, size.height);
-
-    canvas.drawLine(vLineStart, vLineEnd, coordinateLinePaint);
-    canvas.drawLine(hLineStart, hLineEnd, coordinateLinePaint);
-
-    var totalBars = data.length;
-    var barWidth = (size.width - 32) / totalBars;
-    var totalWidthWithSpace = barWidth + 2;
-
-    var maxLabelsValue = data.reduce(max);
-
-    // We need to get height for single data value
-    // Which we can use for bar height
-    // We can calculate bar height in `calculatBarHeight` function below
-    var heightPerValue = size.height / maxLabelsValue;
-
-    // Calculate height for given X value
-    // e.g if value is 2 means height of bar will
-    // be 2 multiply by heightPerValue which is 20
-    // of screen height
-    double calculatBarHeight(int value) {
-      return value * heightPerValue;
+    // Draw vertical coordinate lines
+    for (int i = 0; i <= data.length; i++) {
+      double x = i * barWidth;
+      canvas.drawLine(Offset(x, 0), Offset(x, chartHeight), paint);
     }
 
-    var paddingAtFirstBar = 12;
+    // Draw horizontal coordinate lines
+    for (int i = 0; i <= maxBarHeight; i += 25) {
+      double y = chartHeight - (i * unitHeight);
+      canvas.drawLine(Offset(0, y), Offset(chartWidth, y), paint);
+
+      TextSpan span = TextSpan(
+        text: i.toString(),
+        style: TextStyle(color: Colors.black, fontSize: 12),
+      );
+
+      TextPainter tp = TextPainter(
+        text: span,
+        textAlign: TextAlign.right,
+        textDirection: TextDirection.ltr,
+      );
+      tp.layout();
+      tp.paint(canvas, Offset(-25, y - tp.height / 2));
+    }
 
     // Draw bars
-    for (double i = 0; i < totalBars; i++) {
-      var barHeight = calculatBarHeight(
-          data[i.toInt()]); //size.height * (data[i.toInt()] / 100);
-
-      var barOffsetX = i * totalWidthWithSpace + paddingAtFirstBar;
-
-      var rect = Offset(barOffsetX, size.height - barHeight) &
-          Size(barWidth * 1.0, barHeight);
-      canvas.drawRect(rect, barPaint);
-
-      // Draw X-axis lines for labels
-      canvas.drawLine(Offset(barOffsetX - 1, size.height - 10),
-          Offset(barOffsetX - 1, size.height), coordinateLinePaint);
+    for (int i = 0; i < data.length; i++) {
+      double barHeight = data[i] * unitHeight;
+      double x = i * barWidth;
+      double y = chartHeight - barHeight;
+      Rect barRect = Rect.fromLTWH(x, y, barWidth, barHeight);
+      canvas.drawRect(barRect, paint);
     }
-
-    // Draw Y-axis lines for labels
-    for (double i = 0; i <= size.height; i += heightPerValue) {
-      canvas.drawLine(Offset(0, i), Offset(10, i), coordinateLinePaint);
-    }
-
-    print(totalBars);
-    print(barWidth);
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
+    return false;
   }
 }
